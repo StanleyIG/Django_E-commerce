d18db90a2369a0ed1dc4d0ab2fea8ca28ca57ab8
StanleyIG <104508407+StanleyIG@users.noreply.github.com>
2024-05-13 12:50:38 +0300

    add index page shop махгазин купи продай и убегай (#4)

--- mainapp/views.py
+++ mainapp/views.py
@@ -7,27 +7,9 @@ from django.views.generic import RedirectView
 
 
 def index(request):
-    # return render(request, 'mainapp/index.html')
-    url_1 = reverse('mainapp:index')
-    url_2 = reverse_lazy('mainapp:about')
-    # url_3 = reverse('mainapp:none') 
-    # reverse вычисляет url сразу, даже если он не будет использоваться, и соответсвенно выбросится ошибка, т.к. шаблон url не определён в urls.py
-    url_3 = reverse_lazy('mainapp:none') # ложный адрес которого нет в URLconf.
-    # reverse_lazy вычисляет url лениво, и если его не вызвать, то ошибок не будет, но если например попробовать напечатать, то выбросится ошибка
-    # print(url_3)
-
-    # Допустим ситуация когда есть какая-то проверка (валидация входящих данных, иная логика) 
-    # и ветвление, где есть разный исход результат выполнения view, и каждый 
-    # исход в зависимости от логики предcтавления редиректает на разные адреса.
-    # Получается что есть смысл использовать reverse_lazy для оптимизации выполнения view, что-бы избежать постоянных ненужных 
-    # вычислений url которые могут быть не вызваны 
-
-
-    return HttpResponse('URL1=%s & URL2=%s' % (url_1, url_2))
+    return render(request, 'mainapp/index.html')
 
 
 def about(request):
     url = reverse_lazy('mainapp:index')
     return redirect(url)
-    #return HttpResponse(url)
-    # return render(request, 'mainapp/about.html')
