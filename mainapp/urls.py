28c502620bd75a67e2b8ad560ad99ee164514a11
StanleyIG <104508407+StanleyIG@users.noreply.github.com>
2024-05-13 14:22:28 +0300

    магазин у жорика, ооо "пиво&кириешки", написано на питухоне (#5)

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -6,5 +6,6 @@ app_name = MainappConfig.name
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
+    path('products/', views.products, name='products'),
+    path('contact/', views.contact, name='contact'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,15 +1,50 @@
-from django.http import HttpResponse
-from django.shortcuts import redirect, render
-from django.urls import reverse, reverse_lazy
-from django.views.generic import RedirectView
+from django.shortcuts import render
 
-# Create your views here.
+from mainapp.models import ProductCategory
 
 
 def index(request):
-    return render(request, 'mainapp/index.html')
+    context = {
+        # 'page_title': 'главная',
+    }
+    return render(request, 'mainapp/index.html', context)
 
 
-def about(request):
-    url = reverse_lazy('mainapp:index')
-    return redirect(url)
+def products(request):
+    categories = ProductCategory.objects.all()
+
+    context = {
+        'page_title': 'каталог',
+        'categories': categories,
+    }
+    return render(request, 'mainapp/products.html', context)
+
+
+def contact(request):
+    # from json, from pickle, from db
+    locations = [
+        {
+            'city': 'Москва',
+            'phone': '+7-888-888-8888',
+            'email': 'info@shop.ru',
+            'address': 'В пределах МКАД',
+        },
+        {
+            'city': 'Санкт-Петербург',
+            'phone': '+7-555-333-1111',
+            'email': 'spb@shop.ru',
+            'address': 'В пределах КАД',
+        },
+        {
+            'city': 'Хабаровск',
+            'phone': '+7-444-777-3333',
+            'email': 'mail@shop.ru',
+            'address': 'Центр',
+        }
+    ]
+
+    context = {
+        'page_title': 'контакты',
+        'locations': locations,
+    }
+    return render(request, 'mainapp/contact.html', context)
