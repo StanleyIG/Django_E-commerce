364af8185d669342fa972ae13f5c80a4cc02648d
StanleyIG <104508407+StanleyIG@users.noreply.github.com>
2024-05-18 19:01:27 +0300

    Stage 9 (#9)
    * fix code
    
    * добавил Celery для отправки письма активации

--- authapp_custom/signals.py
+++ authapp_custom/signals.py
@@ -1,5 +1,6 @@
 from django.dispatch import Signal, receiver
-from .utilities import send_activation_notification
+# from .utilities import send_activation_notification
+from .tasks import send_activation_notification_task
 
 
 post_register = Signal()
@@ -7,6 +8,9 @@ post_register = Signal()
 
 @receiver(post_register)
 def post_register_dispatcher(sender, **kwargs):
-    #print('Сигнал сработал')
-    #print(kwargs)
-    send_activation_notification(kwargs['instance'])
+    print('Сигнал сработал')
+    email, username = kwargs['email'], kwargs['username']
+    # send_activation_notification(kwargs['instance'])
+    # send_activation_notification_task.delay(kwargs) таска Celery ожидает простые типы данных python, но QurySet например
+    send_activation_notification_task.delay(email, username)
+    
---
+++ authapp_custom/tasks.py
@@ -0,0 +1,12 @@
+import logging
+from typing import Dict, Union
+from celery import shared_task
+from django.contrib.auth import get_user_model
+from django.core.mail import send_mail
+from .utilities import send_activation_notification, send_activation_notification_user_id
+
+
+@shared_task
+def send_activation_notification_task(email, username):
+    send_activation_notification_user_id(email, username)
+    #send_activation_notification(user['instance'])
