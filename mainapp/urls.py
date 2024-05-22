0db572c650e423fceaef4b20fc21c5f814362c35
StanleyIG <oootehts@gmail.com>
2024-05-22 16:48:28 +0300

    fix code

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,11 +1,21 @@
-from django.urls import path
+from django.urls import path, re_path
 from mainapp import views
 from mainapp.apps import MainappConfig
 
 app_name = MainappConfig.name
 
+# urlpatterns = [
+#     path('', views.index, name='index'),
+#     path('products/', views.products, name='products'),
+#     path('contact/', views.contact, name='contact'),
+# ]
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('products/', views.products, name='products'),
+    # path('products/category/<int:pk>/', mainapp.products, name='catalog'),
+    # path('category/<int:pk>/products/', mainapp.products, name='catalog'),
+    re_path(r'^category/(?P<pk>\d+)/products/$', views.catalog, name='catalog'),
+
     path('contact/', views.contact, name='contact'),
 ]
