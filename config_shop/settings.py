83ab451d034b4ed4a906be7c26c53fbf71b84143
StanleyIG <oootehts@gmail.com>
2024-05-24 18:56:36 +0300

    fix code

--- config_shop/settings.py
+++ config_shop/settings.py
@@ -191,7 +191,9 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 JSON_PATH = 'json'
 
-LOGIN_URL = '/auth/'
+# LOGIN_URL = '/auth/'
+LOGIN_URL = 'auth:login'
+LOGOUT_URL = 'auth:logout'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
